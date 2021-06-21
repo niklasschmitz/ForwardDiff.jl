d0ef58416e5a48fe5270c90523f7199c77bfa00d
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2021-06-21 14:15:39 +0200

    add Dual-scaling of fft plans over Duals

--- src/fft.jl
+++ src/fft.jl
@@ -21,27 +21,27 @@ AbstractFFTs.realfloat(d::ForwardDiff.Dual{T,V,N}) where {T,V,N} = convert(Forwa
 for plan in [:plan_fft, :plan_ifft, :plan_bfft]
     @eval begin
 
-        AbstractFFTs.$plan(x::AbstractArray{<:ForwardDiff.Dual}, region=1:ndims(x)) =
-            AbstractFFTs.$plan(ForwardDiff.value.(x) .+ 0im, region)
+        AbstractFFTs.$plan(x::AbstractArray{<:ForwardDiff.Dual}, region=1:ndims(x); kwargs...) =
+            AbstractFFTs.$plan(ForwardDiff.value.(x) .+ 0im, region; kwargs...)
 
-        AbstractFFTs.$plan(x::AbstractArray{<:Complex{<:ForwardDiff.Dual}}, region=1:ndims(x)) =
-            AbstractFFTs.$plan(ForwardDiff.value.(x), region)
+        AbstractFFTs.$plan(x::AbstractArray{<:Complex{<:ForwardDiff.Dual}}, region=1:ndims(x); kwargs...) =
+            AbstractFFTs.$plan(ForwardDiff.value.(x), region; kwargs...)
 
     end
 end
 
 # rfft only accepts real arrays
-AbstractFFTs.plan_rfft(x::AbstractArray{<:ForwardDiff.Dual}, region=1:ndims(x)) =
-    AbstractFFTs.plan_rfft(ForwardDiff.value.(x), region)
+AbstractFFTs.plan_rfft(x::AbstractArray{<:ForwardDiff.Dual}, region=1:ndims(x); kwargs...) =
+    AbstractFFTs.plan_rfft(ForwardDiff.value.(x), region; kwargs...)
 
 for plan in [:plan_irfft, :plan_brfft]  # these take an extra argument, only when complex?
     @eval begin
 
-        AbstractFFTs.$plan(x::AbstractArray{<:ForwardDiff.Dual}, region=1:ndims(x)) =
-            AbstractFFTs.$plan(ForwardDiff.value.(x) .+ 0im, region)
+        AbstractFFTs.$plan(x::AbstractArray{<:ForwardDiff.Dual}, region=1:ndims(x); kwargs...) =
+            AbstractFFTs.$plan(ForwardDiff.value.(x) .+ 0im, region; kwargs...)
 
-        AbstractFFTs.$plan(x::AbstractArray{<:Complex{<:ForwardDiff.Dual}}, d::Integer, region=1:ndims(x)) =
-            AbstractFFTs.$plan(ForwardDiff.value.(x), d, region)
+        AbstractFFTs.$plan(x::AbstractArray{<:Complex{<:ForwardDiff.Dual}}, d::Integer, region=1:ndims(x); kwargs...) =
+            AbstractFFTs.$plan(ForwardDiff.value.(x), d, region; kwargs...)
 
     end
 end
@@ -55,6 +55,11 @@ for P in [:Plan, :ScaledPlan]  # need ScaledPlan to avoid ambiguities
         Base.:*(p::AbstractFFTs.$P, x::AbstractArray{<:Complex{<:ForwardDiff.Dual}}) =
             _apply_plan(p, x)
 
+        LinearAlgebra.mul!(Y::AbstractArray, p::AbstractFFTs.$P, X::AbstractArray{<:ForwardDiff.Dual}) = 
+            (Y .= _apply_plan(p, X))
+        
+        LinearAlgebra.mul!(Y::AbstractArray, p::AbstractFFTs.$P, X::AbstractArray{<:Complex{<:ForwardDiff.Dual}}) =
+            (Y .= _apply_plan(p, X))
     end
 end
 
@@ -72,3 +77,6 @@ function _apply_plan(p::AbstractFFTs.Plan, x::AbstractArray)
     end
 end
 
+function _apply_plan(p::AbstractFFTs.ScaledPlan{T,P,<:ForwardDiff.Dual}, x::AbstractArray) where {T,P}
+    _apply_plan(p.p, p.scale * x) # for when p.scale is Dual, need out-of-place
+end
--- test/FFTTest.jl
+++ test/FFTTest.jl
@@ -1,6 +1,7 @@
 module FFTTest
 
 using Test
+using ForwardDiff
 using ForwardDiff: Dual, valtype, value, partials
 using FFTW
 using AbstractFFTs: complexfloat, realfloat
@@ -21,6 +22,13 @@ x1 = Dual.(1:4.0, 2:5, 3:6)
     @test partials.(f(x1), 1) == f(partials.(x1, 1))
 end
 
-
+@testset "Dual * Plan over Duals" begin
+   s = ForwardDiff.Dual(2.0, 1.0)
+   x = Dual.(1:5.0, 2:6.0)
+   p = s * FFTW.plan_fft(x)
+   @test typeof(p*x) == Vector{Complex{ForwardDiff.Dual{Nothing, Float64, 1}}}
+   @test partials.(s * fft(x)) ≈ partials.(p * x)
+   @test value.(s * fft(x)) ≈ value.(p * x)
+end
 
 end # module
